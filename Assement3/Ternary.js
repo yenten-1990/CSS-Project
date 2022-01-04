195c2d19107ec2dfac418361d663de0d5b618f23
yenten-1990 <96816802+yenten-1990@users.noreply.github.com>
2022-01-04 21:04:40 +0530

    Add files via upload

---
+++ Assement3/Ternary.js
@@ -0,0 +1,8 @@
+var a=10;
+var b=20;
+//ternary operation 
+var res=20>10?"true" : "false";
+console.log(res);
+const res=(b>a)? "true" : "false";
+const res_1=(b<a)? "true" : "false";
+console.log(res_1);
